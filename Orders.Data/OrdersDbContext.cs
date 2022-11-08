5fc220ef6f1d9b86cdbc5f081978b8f7e91e59cd
Tarxos <ftarxos@gmail.com>
2022-11-08 03:33:05 +0300

    configure main constraints for database

--- Orders.Data/OrdersDbContext.cs
+++ Orders.Data/OrdersDbContext.cs
@@ -9,4 +9,33 @@ public class OrdersDbContext : DbContext
     public DbSet<Order> Orders { get; set; } = null!;
     public DbSet<OrderItem> OrderItems { get; set; } = null!;
     public DbSet<Provider> Providers { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(b =>
+        {
+            // Orders has OrderItems
+            b.HasMany(x => x.OrderItems).WithOne(x => x.Order);
+            // Order has Provider
+            b.HasOne(x => x.Provider).WithMany().HasForeignKey(x=>x.ProviderId);
+            // Order Number and ProviderId is unique constraint
+            b.HasIndex(x => new { x.ProviderId, x.Number }).IsUnique();
+
+            // Order Name cannot be equals to Order Number
+
+            // Order Number, Date, ProviderId is indexed
+            b.HasIndex(x => x.Number);
+            b.HasIndex(x => x.Date);
+        });
+
+        modelBuilder.Entity<OrderItem>(b =>
+        {
+            // OrderItem Name, Unit is indexed
+            b.HasIndex(x => x.Name);
+            b.HasIndex(x => x.Unit);
+        });
+
+        // Provider Name is indexed
+        modelBuilder.Entity<Provider>(b => b.HasIndex(x => x.Name));
+    }
 }
