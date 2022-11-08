1514d66b1d6322ebc79f81a01a0e8e04f61b0721
Tarxos <ftarxos@gmail.com>
2022-11-08 16:07:24 +0300

    configure columns constraints and add migration for Orders data models

--- Orders.Data/Order.cs
+++ Orders.Data/Order.cs
@@ -7,7 +7,7 @@ public sealed class Order
     public string Number { get; set; } = string.Empty;
     public DateTime Date { get; set; }
 
-    public int ProviderId { get; set; }
+    internal int ProviderId { get; set; }
     public Provider Provider { get; set; } = null!;
     public ICollection<OrderItem> OrderItems { get; init; } = new HashSet<OrderItem>();
 }
--- Orders.Data/OrdersDbContext.cs
+++ Orders.Data/OrdersDbContext.cs
@@ -18,10 +18,15 @@ public class OrdersDbContext : DbContext
             b.HasMany(x => x.OrderItems).WithOne(x => x.Order);
             // Order has Provider
             b.HasOne(x => x.Provider).WithMany().HasForeignKey(x=>x.ProviderId);
+
+            // Date has precision in milliseconds with 7
+            b.Property(x => x.Date).HasPrecision(7);
+
+            // Order Name length max as possible
+            b.Property(x => x.Number).HasMaxLength(int.MaxValue);
+
             // Order Number and ProviderId is unique constraint
             b.HasIndex(x => new { x.ProviderId, x.Number }).IsUnique();
-
-            // Order Name cannot be equals to Order Number
 
             // Order Number, Date, ProviderId is indexed
             b.HasIndex(x => x.Number);
@@ -30,12 +35,28 @@ public class OrdersDbContext : DbContext
 
         modelBuilder.Entity<OrderItem>(b =>
         {
-            // OrderItem Name, Unit is indexed
+            // Quantity has precision 18, 3
+            b.Property(x => x.Quantity).HasPrecision(18, 3);
+
+            // OrderItem Name and Unit length max as possible
+            b.Property(x => x.Name).HasMaxLength(int.MaxValue);
+            b.Property(x => x.Unit).HasMaxLength(int.MaxValue);
+
+            // OrderItem Name cannot be equals to Order Number
+            // may be handled by https://www.nuget.org/packages/EFCore.CheckConstraints nuget package extension
+
+            // Name, Unit is indexed
             b.HasIndex(x => x.Name);
             b.HasIndex(x => x.Unit);
         });
 
-        // Provider Name is indexed
-        modelBuilder.Entity<Provider>(b => b.HasIndex(x => x.Name));
+        modelBuilder.Entity<Provider>(b =>
+        {
+            // Provider Name length max as possible
+            b.Property(x => x.Name).HasMaxLength(int.MaxValue);
+
+            // Provider Name is indexed
+            b.HasIndex(x => x.Name);
+        });
     }
 }
